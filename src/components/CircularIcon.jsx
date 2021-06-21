91d4f3d89cde7461660299d00473b5d31129b561
Rohit Kumar <rohitkumar377024@gmail.com>
2021-06-21 20:04:53 +0530

    footer fix new

--- src/components/CircularIcon.jsx
+++ src/components/CircularIcon.jsx
@@ -4,15 +4,17 @@ const CircularIcon = ({ icon, hoverIcon, link }) => {
   const [hover, setHover] = useState(false);
 
   return (
-    <a href={link}>
-      <img
-        onMouseEnter={() => setHover(true)}
-        onMouseLeave={() => setHover(false)}
-        className={`circular-icon mx-3`}
-        src={!hover ? icon : hoverIcon}
-        alt="Circular Icon"
-      />
-    </a>
+    <img
+      onClick={e => {
+        e.preventDefault();
+        window.location.href = link;
+      }}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+      className={`circular-icon mx-3 cursor-pointer`}
+      src={!hover ? icon : hoverIcon}
+      alt="Circular Icon"
+    />
   );
 };
 
