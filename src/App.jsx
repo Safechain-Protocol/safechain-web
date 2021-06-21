96921097c2c11d893d095ca045b368ab75c102c8
Rohit Kumar <rohitkumar377024@gmail.com>
2021-06-21 20:26:24 +0530

    aos check;

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,13 @@ import "aos/dist/aos.css";
 
 function App() {
   useEffect(() => {
-    AOS.init({
-      // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-      offset: 120, // offset (in px) from the original trigger point
-      delay: 0, // values from 0 to 3000, with step 50ms
-      duration: 1200, // values from 0 to 3000, with step 50ms
-      easing: "ease", // default easing for AOS animations
-    });
+    // AOS.init({
+    //   // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    //   offset: 120, // offset (in px) from the original trigger point
+    //   delay: 0, // values from 0 to 3000, with step 50ms
+    //   duration: 1200, // values from 0 to 3000, with step 50ms
+    //   easing: "ease", // default easing for AOS animations
+    // });
   });
 
   return (
